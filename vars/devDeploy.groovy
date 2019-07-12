487e8af18b8418081b84479cf007381c3a3667d4
M1039304 <M1039304@A2ML23760.mindtree.com>
2019-07-12 13:18:49 +0530

    update

--- vars/devDeploy.groovy
+++ vars/devDeploy.groovy
@@ -1,4 +1,4 @@
-def call (def source, def dest, def IP,def user){
-	sh " scp ${source} ${user}@${IP}:${dest}"
+def call (def source, def dest, def devid){
+	sh " scp ${source} ${devid}:${dest}"
 
 }
