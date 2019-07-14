0125b7192ad536376106234152a21fd87bf4f624
M1039304 <M1039304@A2ML23760.mindtree.com>
2019-07-14 11:30:18 +0530

    commit8

--- vars/devDeploy.groovy
+++ vars/devDeploy.groovy
@@ -1,10 +1,7 @@
 def call(def hosturl,def user, def dest){
 
 //	sh "ssh ${user}@${hosturl}"
-	sh " ssh ubuntu@52.10.25.211"
-	sh " cd ${dest}"
-	sh " lsof -ti tcp:8080 | xargs kill"
-	sh " java -jar *.jar"
-	sh " exit" 
+	sh " ssh -tt ubuntu@52.10.25.211 fuser -k 8080/tcp "
+    sh " ssh -tt ubuntu@52.10.25.211 java -jar *.jar "
 
 }
