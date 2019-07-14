c345b27329aba4dc726e3bb3079e31f384fa0800
M1039304 <M1039304@A2ML23760.mindtree.com>
2019-07-14 14:29:53 +0530

    commit 2

--- vars/devDeploy.groovy
+++ vars/devDeploy.groovy
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
 def call(def hosturl,def user, def dest){
 
 //	sh "ssh ${user}@${hosturl}"
 	sh " ssh -tt ubuntu@52.10.25.211 fuser -k 8080/tcp "
     sh " ssh -tt ubuntu@52.10.25.211 java -jar *.jar "
+=======
+def call (def source, def dest, def devid){
+	sh " scp ${source} ${devid}:${dest}"
+>>>>>>> branch 'master' of https://github.com/shruti-singh26/JenkinsSharedLib.git
 
 }
